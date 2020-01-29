b9b38fc904054578ad15d0d7a280c5dee9245021
Killian <killian.babilotte@gmail.com>
2020-01-29 21:40:25 +0100

    ajout com killian

--- test.c
+++ test.c
@@ -23,10 +23,12 @@ typedef struct chunk_t
 	size_t chunk_size;			// taille du chunk en octet
 	char allocated;				// indicateur d'allocation 
 
-	struct chunk_t *previous;	// pointeur vers le chunk precedent du block (NULL si tete de la liste)
+	struct chunk_t *previous;	// pointeur vers le chunk precedent du block (NULL si tete de la liste) // :') j'aurais fais l'inverse xD
 	struct chunk_t *next;		// pointeur vers le chunk suivant du block (NULL si queue de la liste)
 
-	struct chunk_t *tab_next;	// pointeur vers le chunk suivant de la table de hachage (NULL si queue de la liste)
+	struct chunk_t *tab_next;	// pointeur vers le chunk suivant de la table de hachage (NULL si queue de la liste) // de la table de hash ?
+                                        // ça veut dire que tu répertorie sur les chuncks où est le prochain libre ?
+                                        // si oui ça me parait couteux à mettre à jour comme info mais je me trompe peut etre
 	
 	void *adress;				// adresse de l'espace mémoire correspondant a ce chunk
 }chunk_t;
@@ -40,13 +42,13 @@ void *malloc(size_t)
 	/* Etape 1 Chercher dans la table de hachage si un chunk convient */
 		/* Etape 1.1 si oui */
 			/* Etape 1.1.1 Le scinder dans la liste des chunks du blocks */
-			/* Etape 1.1.2 Ajouter les deux chunks dans la table de hachage */
-			/* Etape 1.1.3 Supprimer l'ancien chunk de la table de hachage */
+			/* Etape 1.1.2 Ajouter les deux chunks dans la table de hachage */  // les 2 ? :o pas juste le libre ?
+                        /* Etape 1.1.3 Supprimer l'ancien chunk de la table de hachage */    
 			/* Etape 1.1.4 Retourner le chunk de taille convenu */
 		/* Etape 1.2 si non */
 			/* Etape 1.1.1 Allouer un chunk de taille >= MIN_BLOCK_SIZE avec mmap*/
-			/* Etape 1.1.2 Scinder ce block si necessaire dans la liste de chunk */
-			/* Etape 1.1.3 Ajouter les deux chunks dans la table de hachage */
+			/* Etape 1.1.2 Scinder ce block si necessaire dans la liste de chunk */ //cas si < MIN_BLOCK_SIZE
+                        /* Etape 1.1.3 Ajouter les deux chunks dans la table de hachage */ //On ajoute les blocks occupé ???
 			/* Etape 1.1.4 Retourner le chunk de taille convenu */
 
 	return NULL;
@@ -55,12 +57,13 @@ void *malloc(size_t)
 void free(void *ptr)
 {
 	/* Etape 1 Chercher dans la table de hachage un chunk a l'adresse voulu */
-		/* Etape 1.1 si il y en a un */
+                /* Etape 1.1 si il y en a un */ //et que ce n'est pas un block entier et que > MAX_FREE_CAP //pour éviter de garder trop de mem ?
 			/* Etape 1.1.1 le desallouer */
 			/* Etape 1.1.2 le fusionner avec le chunk precedent dans le bloc si possible */
 			/* Etape 1.1.3 le fusionner avec le chunk suivant dans le bloc si possible */
 			/* Etape 1.1.4 si à la fin le chunk est de la meme taille que le block et qu'il n'a pas de predecesseur */
-				/* Etape 1.1.4.1 alors unmap le chunk car il représente un block vide */
+                               /* Etape 1.1.4.1 alors unmap le chunk car il représente un block vide */ //ah je viens de voir que tu fais ça là xD
+                // Etape 1.2 unmap le block si on fait ma version
 }
 
 void *calloc(size_t nmemb, size_t size);
@@ -68,7 +71,7 @@ void *calloc(size_t nmemb, size_t size);
 
 	/* Etape 1 Allouer un chunk de taille >= MIN_BLOCK_SIZE avec mmap*/
 	/* Etape 2 Scinder ce block si necessaire dans la liste de chunk */
-	/* Etape 3 Ajouter les deux chunks dans la table de hachage */
+	/* Etape 3 Ajouter les deux chunks dans la table de hachage */ // toujours cette histoire de block occupé qui est ajouté au hash qui me dérange
 	/* Etape 4 Retourner le chunk de taille convenu */
 
 	return NULL;
@@ -81,12 +84,12 @@ void *realloc(void *ptr, size_t size)
 			/* Etape 1.1.1 Le scinder dans la liste des chunks du blocks */
 			/* Etape 1.1.2 Ajouter les deux chunks dans la table de hachage */
 			/* Etape 1.1.3 Supprimer l'ancien chunk de la table de hachage */
-		/* Etape 1.2 si non */
+                /* Etape 1.2 si non */ //realloc fait du malloc si y a pas de block ??? :o
 			/* Etape 1.1.1 Allouer un chunk de taille >= MIN_BLOCK_SIZE avec mmap */
 			/* Etape 1.1.2 Scinder ce block si necessaire dans la liste de chunk */
 			/* Etape 1.1.3 Ajouter les deux chunks dans la table de hachage */
 	/* Etape 2  copier le contenu de l'ancien chunk dans le nouveau chunk avec memcpy*/
-	/* Etape 3 desallouer l'ancien chunk */
+	/* Etape 3 desallouer l'ancien chunk */ //utiliser free --> etape 3 à 6.1
 	/* Etape 4 le fusionner avec le chunk precedent dans le bloc si possible */
 	/* Etape 5 le fusionner avec le chunk suivant dans le bloc si possible */
 	/* Etape 6 si à la fin le chunk est de la mme taille que le block et qu'il n'a pas de predecesseur */
@@ -97,15 +100,15 @@ void *realloc(void *ptr, size_t size)
 }
 
 /********************************************************************************************
-*									Fonctions utiles										*
-*																							*
+*								       Fonctions utiles										*
+*							       													*
 *	chunk_t *recherche_chunk(size_t size);													*
 *		retourne un pointeur vers un chunk de taille >= size ou NULL si il n'y en pas dans 	*
 *		la table de hachage 																*
-*																							*	
+*																				*
 *	chunk_t	*merge_free(chunk_t *c);														*
 *		fusionne un chunk avec ces voisins libres et retourne l'adresse du nouveau chunk	*
-*																							*
+*																				*
 ********************************************************************************************/	
 
 
