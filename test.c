4c731be786dfdb05b200393bbee6c0c5af337ede
Avaquet <alan.vaquet@gmail.com>
2020-01-29 20:55:22 +0100

    explication du taff a faire

--- test.c
+++ test.c
@@ -1,341 +1,119 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <assert.h>
 #include <string.h>
+#include <math.h>
 
-/* structures de donneés */
-
-typedef struct block block_t;
-typedef struct chunk chunk_t;
-typedef struct bin bin_t;
-
-struct block{
-	size_t size;
-	chunk_t *list;
-	block_t *next;
-};
-
-struct chunk{
-	size_t size;
-	char allocated;
-	chunk_t *previous;
-	chunk_t *next;
-	void *adr;
-};
-
-static block_t *MEM = NULL;
-
-static void *add_block(size_t block_size);
-static void del_block(block_t *adr);
-static void print_MEM();
-static void *add_chunk(size_t chunk_size);
-static void del_chunk(void *adr);
-static void *move_chunk(void *adr, size_t chunk_newsize);
-static void print_list();
-
-
-/* Gestion des blocks */
-
-#define SMALLEST_SIZE 4096
-
-#define BIGEST_SIZE 2147483648
-
-static void *add_block(size_t block_size)
+/* Fonction de hachage permettant d'acceder aux chunks */
+static inline int fct_hachage(size_t size)
 {
-	/* allocation des metadata du block */
-	block_t *tmp = (block_t*)mmap(0, sizeof(block_t), PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	tmp->next = MEM;
-
-	/* allocatin des metadata du chunk d'origine */
-	tmp->list = (chunk_t*)mmap(0, sizeof(chunk_t), PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	chunk_t *l = tmp->list;
-
-	/* allocation de l'espace mémoire dans le chunk d'origine */
-	if(block_size < SMALLEST_SIZE){
-		l->adr = mmap(0, SMALLEST_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-		l->size = SMALLEST_SIZE;
-		tmp->size = SMALLEST_SIZE;
-	}else if(block_size > BIGEST_SIZE){
-		l->adr = mmap(0, BIGEST_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-		l->size = BIGEST_SIZE;
-		tmp->size = BIGEST_SIZE;
+	if(size >= 512){
+		return log2(size+1)*7;/* OK */
 	}else{
-		l->adr = mmap(0, block_size, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-		l->size = block_size;
-		tmp->size = block_size;
+		return size>>3;	/* OK */
 	}
-
-	/* initialisation du chunk d'origine */
-	l->allocated = 0;
-	l->previous = NULL;
-	l->next = NULL;
-
-	MEM = tmp;
-	return tmp;
 }
 
-static void del_block(block_t *adr)
+/* Structure d'un chunk */
+typedef struct chunk_t
 {
-	block_t *actual = MEM, *pred = NULL;
-	chunk_t *c = actual->list, *n = NULL;
+	size_t block_size;			// taille du block auquel appartient le chunk en octet
+	size_t chunk_size;			// taille du chunk en octet
+	char allocated;				// indicateur d'allocation 
 
-	/* Si l'adresse est celle du premier block */
-	if(MEM == adr){
-		MEM = actual->next;
-		/* 1 suppression de l'esapce memoire du block contenu à l'adresse pointée par le premier chunk*/ 
-		munmap(actual->list->adr, actual->size);
-		/* 2 suppression de tous les chunk de la liste du block */ 
-		while(c != NULL){
-			n = c->next;
-			munmap(c,sizeof(chunk_t));
-			c = n;}
-		/* 3 suppression du block*/
-		munmap(actual,sizeof(block_t));
-		return;}
+	struct chunk_t *previous;	// pointeur vers le chunk precedent du block (NULL si tete de la liste)
+	struct chunk_t *next;		// pointeur vers le chunk suivant du block (NULL si queue de la liste)
 
-	/* Sinon parcourir la list de block jusqu'a trouver l'adresse */
-	while(actual != adr && actual != NULL){
-		pred = actual;
-		actual = actual->next;}
+	struct chunk_t *tab_next;	// pointeur vers le chunk suivant de la table de hachage (NULL si queue de la liste)
+	
+	void *adress;				// adresse de l'espace mémoire correspondant a ce chunk
+}chunk_t;
 
-	/* si l'adresse à été trouvé alors supprimer le block */
-	if(actual != NULL){
-		pred->next = actual->next;
-		/* 1 suppression de l'esapce memoire du block contenu à l'adresse pointée par le premier chunk*/ 
-		munmap(actual->list->adr, actual->size);
-		/* 2 suppression de tous les chunk de la liste du block */ 
-		chunk_t *c = actual->list, *n = NULL;
-		while(c != NULL){
-			n = c->next;
-			munmap(c,sizeof(chunk_t));
-			c = n;}
-		/* 3 suppression du block*/
-		munmap(actual,sizeof(block_t));
-		return;}
-}
+/* Table de hachage */
+static chunk_t *tab[218] = {};
 
-static void print_MEM()
+/* Les fonctions qui nous interessent */
+void *malloc(size_t)
 {
-	block_t *actual = MEM;
+	/* Etape 1 Chercher dans la table de hachage si un chunk convient */
+		/* Etape 1.1 si oui */
+			/* Etape 1.1.1 Le scinder dans la liste des chunks du blocks */
+			/* Etape 1.1.2 Ajouter les deux chunks dans la table de hachage */
+			/* Etape 1.1.3 Supprimer l'ancien chunk de la table de hachage */
+			/* Etape 1.1.4 Retourner le chunk de taille convenu */
+		/* Etape 1.2 si non */
+			/* Etape 1.1.1 Allouer un chunk de taille >= MIN_BLOCK_SIZE avec mmap*/
+			/* Etape 1.1.2 Scinder ce block si necessaire dans la liste de chunk */
+			/* Etape 1.1.3 Ajouter les deux chunks dans la table de hachage */
+			/* Etape 1.1.4 Retourner le chunk de taille convenu */
 
-	while(actual != NULL){
-		printf("block adress: %zu\n", actual);
-		printf("block size: %zu\n", actual->size);
-		printf("next block: %zu\n", actual->next);
-		printf("list first chunk: %zu\n\n", actual->list);
-		print_list(actual);
-		actual = actual->next;
-	}
-
-	printf("NULL\n");
-	printf("--------------------------------------------------------------------\n");
+	return NULL;
 }
 
-/* Gestion des chunks */
-
-static void* add_chunk(size_t chunk_size)
+void free(void *ptr)
 {
-	block_t *actual = MEM;
-	chunk_t *pt = NULL;
-
-	/* on parcourt les blocks */
-	while(actual != NULL){
-		/* on parcourt les chunks du blocks */
-		pt = actual->list;
-		while(pt != NULL){
-			/* le chunk est de taille correspond a celle demandé et qu'il est libre */
-			if(pt->size >= chunk_size && pt->allocated == 0)
-				break;
-			else
-				pt = pt->next;
-		}
-		if(pt != NULL)
-			break;
-		else
-			actual = actual->next;
-	}
-
-	/* 1 un chunk de taille suffisante est trouvé */
-	if(pt != NULL){
-		/* si il ne restera pas assez d'espace apres avoir retiré chunk size alors ne pas le faire */
-		if(chunk_size == pt->size){
-			pt->allocated = 1;			
-			return pt->adr;
-		}else{
-			/* création du chunk représentant l'espace restant */
-			chunk_t *new_chunk = (chunk_t*)mmap(0, sizeof(chunk_t), PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-			new_chunk->allocated = 0;
-			new_chunk->size = pt->size - chunk_size;
-			new_chunk->adr = &(*pt->adr) + chunk_size;
-			new_chunk->next = pt->next;
-			new_chunk->previous = pt;
-			/* modification du chunk alloué */
-			pt->allocated = 1;
-			pt->size = chunk_size;
-			pt->next = new_chunk;
-			return pt->adr;
-		}
-	}
-
-	/* 2 aucun chunk de taille suffisante n'à été trouvé */
-	else{
-		actual = add_block(chunk_size);
-		pt = actual->list;
-		/* si il ne restera pas assez d'espace apres avoir retiré chunk size alors ne pas le faire */
-		if(chunk_size == pt->size){
-			pt->allocated = 1;			
-			return pt->adr;
-		}else{
-			/* création du chunk représentant l'espace restant */
-			chunk_t *new_chunk = (chunk_t*)mmap(0, sizeof(chunk_t), PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-			new_chunk->allocated = 0;
-			new_chunk->size = pt->size - chunk_size;
-			new_chunk->adr = &(*pt->adr) + chunk_size;
-			new_chunk->next = pt->next;
-			new_chunk->previous = pt;
-			/* modification du chunk alloué */
-			pt->allocated = 1;
-			pt->size = chunk_size;
-			pt->next = new_chunk;
-			return pt->adr;
-		}
-	}
-
-	printf("\n\nchunk address %zu taille demandé %zu taille du chunk %zu \n\n\n", pt->adr, chunk_size, pt->size);
+	/* Etape 1 Chercher dans la table de hachage un chunk a l'adresse voulu */
+		/* Etape 1.1 si il y en a un */
+			/* Etape 1.1.1 le desallouer */
+			/* Etape 1.1.2 le fusionner avec le chunk precedent dans le bloc si possible */
+			/* Etape 1.1.3 le fusionner avec le chunk suivant dans le bloc si possible */
+			/* Etape 1.1.4 si à la fin le chunk est de la meme taille que le block et qu'il n'a pas de predecesseur */
+				/* Etape 1.1.4.1 alors unmap le chunk car il représente un block vide */
 }
 
-static void del_chunk(void *adr)
+void *calloc(size_t nmemb, size_t size);
 {
-	block_t *actual = MEM;
-	chunk_t *pt = NULL, *old_pt = NULL;
 
-	/* on parcourt les blocks */
-	while(actual != NULL){
-		/* on parcourt les chunks du blocks */
-		pt = actual->list;
-		while(pt != NULL){
-			/* le chunk pointe vers cette adresse */
-			if(pt->adr == adr)
-				break;
-			else
-				pt = pt->next;
-		}
-		if(pt != NULL)
-			break;
-		else
-			actual = actual->next;
-	}
+	/* Etape 1 Allouer un chunk de taille >= MIN_BLOCK_SIZE avec mmap*/
+	/* Etape 2 Scinder ce block si necessaire dans la liste de chunk */
+	/* Etape 3 Ajouter les deux chunks dans la table de hachage */
+	/* Etape 4 Retourner le chunk de taille convenu */
 
-	if(pt != NULL){
-		/* 1 le désalloué */
-		pt->allocated = 0;
-
-		/* 2 le fusionner avec le chunk avant ou apres lui si ils sont libre */
-		if(pt->previous != NULL && pt->previous->allocated == 0){
-			pt->previous->size += pt->size;
-			pt->previous->next = pt->next;
-			if(pt->next != NULL)
-				pt->next->previous = pt->previous;
-			old_pt = pt;
-			pt = pt->previous;
-			munmap(old_pt,sizeof(chunk_t));
-		}
-
-		if(pt->next != NULL && pt->next->allocated == 0){
-			pt->size += pt->next->size;
-			pt->next = pt->next->next;
-			if(pt->next != NULL)
-				pt->next->previous = pt;
-			munmap(pt->next,sizeof(chunk_t));
-		}
-
-	}
+	return NULL;
 }
 
-static void *move_chunk(void	*adr, size_t chunk_newsize)
+void *realloc(void *ptr, size_t size)
 {
-	void *new_adr = add_chunk(chunk_newsize);
+	/* Etape 1 Chercher dans la table de hachage si un chunk convient */
+		/* Etape 1.1 si oui */
+			/* Etape 1.1.1 Le scinder dans la liste des chunks du blocks */
+			/* Etape 1.1.2 Ajouter les deux chunks dans la table de hachage */
+			/* Etape 1.1.3 Supprimer l'ancien chunk de la table de hachage */
+		/* Etape 1.2 si non */
+			/* Etape 1.1.1 Allouer un chunk de taille >= MIN_BLOCK_SIZE avec mmap */
+			/* Etape 1.1.2 Scinder ce block si necessaire dans la liste de chunk */
+			/* Etape 1.1.3 Ajouter les deux chunks dans la table de hachage */
+	/* Etape 2  copier le contenu de l'ancien chunk dans le nouveau chunk avec memcpy*/
+	/* Etape 3 desallouer l'ancien chunk */
+	/* Etape 4 le fusionner avec le chunk precedent dans le bloc si possible */
+	/* Etape 5 le fusionner avec le chunk suivant dans le bloc si possible */
+	/* Etape 6 si à la fin le chunk est de la mme taille que le block et qu'il n'a pas de predecesseur */
+		/* Etape 6.1 alors unmap le chunk car il représente un block vide */
+	/* Etape 7 retourner le nouveau chunk */
 
-	block_t *actual = MEM;
-	chunk_t *pt = NULL;
-
-	/* on parcourt les blocks */
-	while(actual != NULL){
-		/* on parcourt les chunks du blocks */
-		pt = actual->list;
-		while(pt != NULL){
-			/* le chunk pointe vers cette adresse */
-			if(pt->adr == adr)
-				break;
-			else
-				pt = pt->next;
-		}
-		if(pt != NULL)
-			break;
-		else
-			actual = actual->next;
-	}
-
-	/* copie le contenu à la nouvelle adresse et supprimme l'ancienne */
-	if(pt != NULL){
-		memcpy(new_adr, adr, pt->size);
-		del_chunk(adr);
-	}else{
-		return NULL;
-	}
-
-	return new_adr;
+	return NULL;
 }
 
-static void print_list(block_t *actual)
-{
-	chunk_t *pt = actual->list;
+/********************************************************************************************
+*									Fonctions utiles										*
+*																							*
+*	chunk_t *recherche_chunk(size_t size);													*
+*		retourne un pointeur vers un chunk de taille >= size ou NULL si il n'y en pas dans 	*
+*		la table de hachage 																*
+*																							*	
+*	chunk_t	*merge_free(chunk_t *c);														*
+*		fusionne un chunk avec ces voisins libres et retourne l'adresse du nouveau chunk	*
+*																							*
+********************************************************************************************/	
 
-	while(pt != NULL){
-		printf("\tadresse du chunk = %zu\n", pt);
-		printf("\tallocated = %d\n", pt->allocated);
-		printf("\tadresse pointe par le chunk = %zu\n", &(*pt->adr));
-		printf("\ttaille du chunk = %zu\n", pt->size);
-		printf("\tadresse du chunk precedent = %zu\n", pt->previous);
-		printf("\tadresse du suivant = %zu\n\n", pt->next);
-		pt = pt->next;
-	}
-}
+
 
 
 int main(int argc, char const *argv[])
 {
-	block_t *block1 = add_block(454645);
-	block_t *block2 = add_block(21000);
-	block_t *block3 = add_block(50364);
-	block_t *block4 = add_block(89);
-	print_MEM();
 
-	void *pt6 = add_chunk(60000);
-	void *pt5 = add_chunk(50);
-	void *pt4 = add_chunk(4096);
-	void *pt3 = add_chunk(90000);
-	void *pt2 = add_chunk(890);
-	void *pt1 = add_chunk(8888888);
-	print_MEM();
 
-	del_chunk(pt1);
-	print_MEM();
-
-	del_chunk(pt3);
-	print_MEM();
-
-	del_chunk(pt5);
-	print_MEM();
-
-	del_chunk(pt2);
-	print_MEM();
-
-	del_block(block1);
-	del_block(block3);
-	del_block(block4);
-	del_block(block2);
-	print_MEM();
-	printf("pas de panique c normal celui la n'est pas supprimé par nous mais par le systeme\n");
+	return 0;
 }
