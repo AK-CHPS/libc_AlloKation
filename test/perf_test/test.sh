38acd2424230e899d10f2e4f5c8cd8e71bbbf3a0
Avaquet <alan.vaquet@gmail.com>
2020-03-07 14:21:06 +0100

    modification de la compilation dans perf_test.sh

--- test/perf_test/test.sh
+++ test/perf_test/test.sh
@@ -3,5 +3,5 @@
 gcc -D N=200 perf_test.c -o perf_test -O3
 ./perf_test 000000
 
-gcc -D N=200 -D AK perf_test.c -o perf_test -I/usr/local/include/ -L/usr/local/lib/ -lmAlloK -O3
+gcc -D N=200 -D AK perf_test.c -o perf_test -L/usr/local/lib/ -I/usr/local/include/ -lmAlloK -O3
 ./perf_test $i
